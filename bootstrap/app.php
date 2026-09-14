95da70432c16578683ec160238c9224cf3da1d80
Erick Pérez <ericksperezc@gmail.com>
2026-09-14 09:30:42 -0500

    Update .env.docker to change application and database ports; refactor app.php to improve middleware syntax and organization.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -13,21 +13,15 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
-                'role' => RoleMiddleware::class,
-                'permission' => PermissionMiddleware::class,
-                'role_or_permission' => RoleOrPermissionMiddleware::class,
+            'role' => RoleMiddleware::class,
+            'permission' => PermissionMiddleware::class,
+            'role_or_permission' => RoleOrPermissionMiddleware::class,
         ]);
-    Route::middleware(['auth', 'role:líder'])->group(function () {
-
-    Route::get('/team/manage', function () {
-        return view('teams.manage');
-    })->name('teams.manage');
-
-});
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
-    
